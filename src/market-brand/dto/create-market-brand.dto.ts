07089f72807e8989f40b9d5df0b7297ddab2b441
ZioCain <micael.castelleits@gmail.com>
2025-12-16 14:22:11 +0100

    Allow file to be uploaded for the market-brand

--- src/market-brand/dto/create-market-brand.dto.ts
+++ src/market-brand/dto/create-market-brand.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateMarketBrandDto {
 	@ApiProperty({description: "Il nome del brand", example: "Carrefour", type: "string"})
@@ -8,4 +8,9 @@ export class CreateMarketBrandDto {
 	@IsNotEmpty()
 	@Transform(({ value }) => value.trim())
 	readonly name: string;
+
+	@ApiProperty({description: "Il logo del brand", example: "file url", type: "string"})
+	@IsString()
+	@IsOptional()
+	readonly logo: string;
 }
--- src/market-brand/dto/update-market-brand.dto.ts
+++ src/market-brand/dto/update-market-brand.dto.ts
@@ -1,9 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMarketBrandDto } from './create-market-brand.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateMarketBrandDto extends PartialType(CreateMarketBrandDto) {
 	@IsString()
-	@IsNotEmpty()
-	name: string;
+	@IsOptional()
+	readonly name: string;
+
+	@IsString()
+	@IsOptional()
+	readonly logo: string;
 }
